8c74160969964fa80c8dd86ea41cdbee5001a1e3
yzs <yeozhishen@gmail.com>
2024-02-23 17:39:20 +0800

    Add lambdas

--- src/main/java/se/edu/streamdemo/Main.java
+++ src/main/java/se/edu/streamdemo/Main.java
@@ -5,6 +5,9 @@ import se.edu.streamdemo.task.Deadline;
 import se.edu.streamdemo.task.Task;
 
 import java.util.ArrayList;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toList;
 
 public class Main {
     public static void main(String[] args) {
@@ -19,7 +22,11 @@ public class Main {
         printDeadlines(tasksData);
 
         System.out.println("Total number of deadlines: " + countDeadlines(tasksData));
+        System.out.println("printing deadlines using streams");
+        printDeadlinesUsingStreams(tasksData);
 
+        ArrayList<Task> filteredList = filterByString(tasksData, "11");
+        printAllData(filteredList);
     }
 
     private static int countDeadlines(ArrayList<Task> tasksData) {
@@ -44,6 +51,15 @@ public class Main {
                 System.out.println(t);
             }
         }
+    }
+    public static void printDeadlinesUsingStreams(ArrayList<Task> tasks){
+        tasks.stream().filter((t) -> t instanceof Deadline)
+                .sorted((t1,t2) -> t1.getDescription().compareToIgnoreCase(t2.getDescription()))
+                .forEach(System.out::println);
+    }
+    public static ArrayList<Task> filterByString(ArrayList<Task> tasks, String filterString) {
+        ArrayList<Task> filteredList = (ArrayList<Task>) tasks.stream().filter(t -> t.getDescription().contains(filterString)).collect(toList());
+        return filteredList;
     }
 
 }
